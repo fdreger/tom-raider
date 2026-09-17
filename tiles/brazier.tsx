2313db94667aa64ef9cfdc22980c181dc6464f60
Byron Morley <byronmorley6@gmail.com>
2026-09-17 08:09:37 +0100

    tsx

---
+++ tiles/brazier.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="brazier" tilewidth="16" tileheight="32" tilecount="4" columns="4">
+ <image source="../../repos/tom-raider/tiles/brazier.png" width="64" height="32"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ tiles/egypt.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="egypt" tilewidth="8" tileheight="8" tilecount="1512" columns="42">
+ <image source="../pixel-art/egypt.png" width="340" height="288"/>
+</tileset>
